cbb6ca1aa46ea3769a3cc45dec219c3ca38f9ba9
malekhamoud <malek2002hamoud@gmail.com>
2025-07-16 14:57:04 +0300

    landing pagr render

---
+++ src/unAuth/landingpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./landingpage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <h1>Hello Landing Page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
