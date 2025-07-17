7312443500330df481eca4e26c334879d872ac25
malekhamoud <malek2002hamoud@gmail.com>
2025-07-17 20:39:37 +0300

    withlogo2

--- src/unAuth/componnets/navpar.js
+++ src/unAuth/componnets/navpar.js
@@ -1,23 +1,21 @@
 import React from "react";
 import logo from "../../assets/Screenshot.png";
-
-import "./navpar.css"; // or whatever your CSS file is called
+import "./navpar.css";
 
 function Navpar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="Logo" className="logo" />
-        
       </div>
 
-      <ul className="navbar-menu">
-        
-        <li className="menu-item">Pricing</li>
-        <li className="menu-item">Blog</li>
-      </ul>
-
-      <button className="quote-button">Do more with video</button>
+      <div className="navbar-right">
+        <ul className="navbar-menu">
+          <li className="menu-item">Pricing</li>
+          <li className="menu-item">Blog</li>
+        </ul>
+        <button className="quote-button">Do more with video .. </button>
+      </div>
     </nav>
   );
 }
